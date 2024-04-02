4e105aa067763586d0e6b4b84e1b9deaf86e2276
kevpy1 <kevin.c@aluno.ifsp.edu.br>
2024-04-02 15:28:41 -0300

    continuacao

--- ATV5_CJ2011258/Program.cs
+++ ATV5_CJ2011258/Program.cs
@@ -80,12 +80,116 @@ namespace ATV5_CJ2011258
                     break;
 
                 case 4:
-                    Console.WriteLine()
-            
+                    int somaPositivos = 0;
+
+                    Console.WriteLine("Digite uma sequência de números inteiros (digite um número negativo para parar):");
+
+
+                    {
+                        Console.Write("Digite um número inteiro: ");
+                        int nume = int.Parse(Console.ReadLine());
+
+                        if (nume < 0)
+                        {
+                            break;
+                        }
+
+                        if (nume > 0)
+                        {
+                            somaPositivos += nume;
+                            Console.WriteLine("Número positivo adicionado. Soma atual dos positivos: {somaPositivos}");
+                        }
+                    }
+
+                    Console.WriteLine("A soma dos números positivos é: {0}", somaPositivos);
+                    break;
+
+
+                case 5:
+
+                    Console.Write("Digite um número inteiro: ");
+                    int n1 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Os divisores do número {n1} são:");
+
+                    for (int d = 1; d <= n1; d++)
+                    {
+                        if (n1 % d == 0)
+                        {
+                            Console.WriteLine(d);
+
+                        }
+                    }
+                    break;
+
+                case 6:
+
+                    int quantidadeNumeros = 10;
+                    int[] numeros = new int[quantidadeNumeros];
+                    int maior = int.MinValue;
+                    int menor = int.MaxValue;
+
+                    Console.WriteLine("Digite 10 números:");
+
+                    for (int i = 0; i < quantidadeNumeros; i++)
+                    {
+                        Console.Write("Número {i + 1}: ");
+                        numeros[i] = int.Parse(Console.ReadLine());
+
+                        if (numeros[i] > maior)
+                        {
+                            maior = numeros[i];
+                        }
+
+                        if (numeros[i] < menor)
+                        {
+                            menor = numeros[i];
+                        }
+                    }
+
+                    Console.WriteLine("O maior número digitado foi: {0} ", maior);
+                    Console.WriteLine("O menor número digitado foi: {0}", menor);
+                    break;
+
+                case 7:
+
+                    int somaPares = 0;
+                    int somaImpares = 0;
+
+                    Console.WriteLine("Digite uma sequência de números. Digite 0 para encerrar.");
+
+                    {
+                        Console.Write("Digite um número: ");
+                        int n2 = int.Parse(Console.ReadLine());
+
+                        if (n2 == 0)
+                        {
+                            break;
+                        }
+
+                        if (n2 % 2 == 0)
+                        {
+                            somaPares += n2;
+                        }
+                        else
+                        {
+                            somaImpares += n2;
+                        }
+                    }
+
+                    Console.WriteLine("A soma dos números pares é: {0}", somaPares);
+                    Console.WriteLine("A soma dos números ímpares é: {0}", somaImpares);
+                    break;
+
+
             }
         }
     }
 }
+            
+        
+    
+
 
 
 
